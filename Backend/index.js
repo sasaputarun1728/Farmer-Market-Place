5407f51ac4ebb1c5a56a67ff4e2941f400f8abc8
sasaputarun1728 <160327476+sasaputarun1728@users.noreply.github.com>
2024-11-03 17:47:31 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -9,7 +9,7 @@ const port = 5000;
 
 app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.setHeader("Access-Control-Allow-Origin", "https://farmer-market-place.onrender.com");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
